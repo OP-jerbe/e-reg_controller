05f0d6574e51d200e2f5c18f82d5b3a28063116d
OP-jerbe <j.erbe@oregon-physics.com>
2026-01-09 08:20:18 -0800

    Add the helper functions to get the data from the ini file.

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,4 +1,5 @@
 import sys
+from configparser import ConfigParser
 from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
 
@@ -21,3 +22,26 @@ def get_root_dir() -> Path:
         return Path(getattr(sys, '_MEIPASS', '.'))
     else:  # Running in a normal Python environment
         return Path(__file__).resolve().parents[1]
+
+
+def get_ini_filepath() -> Path:
+    root_dir = get_root_dir()
+    ini_filepath = Path(root_dir / 'configuration' / 'config.ini')
+    return ini_filepath
+
+
+def load_ini(ini_file: Path) -> ConfigParser:
+    config_data = ConfigParser()
+    config_data.read(str(ini_file))
+    return config_data
+
+
+def find_selection(config_data: ConfigParser, header: str, selection: str) -> str:
+    return config_data.get(header, f'{selection}')
+
+
+if __name__ == '__main__':
+    # How to get the data in the ini file
+    ini_filepath = get_ini_filepath()
+    config_data = load_ini(ini_filepath)
+    IPAddress = find_selection(config_data, 'IPAddress', 'IPAddress')
