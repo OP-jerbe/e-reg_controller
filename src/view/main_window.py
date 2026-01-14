26bd50b74106f2fb783adf299f69a76d410c0cdd
OP-jerbe <j.erbe@oregon-physics.com>
2026-01-14 07:18:57 -0800

    set up a basic layout for the main window

--- src/view/main_window.py
+++ src/view/main_window.py
@@ -1,4 +1,13 @@
-from PySide6.QtWidgets import QLabel, QLineEdit, QMainWindow
+from PySide6.QtCore import Signal
+from PySide6.QtGui import QCloseEvent
+from PySide6.QtWidgets import (
+    QFrame,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
 from qt_material import apply_stylesheet
 
 import src.helpers.helpers as h
@@ -6,9 +15,12 @@ from src.model.ereg_driver import eReg
 
 
 class MainWindow(QMainWindow):
+    ereg_connected_sig = Signal()
+    closing_sig = Signal()
+
     def __init__(self, model: eReg) -> None:
         super().__init__()
-        self.model = model
+        self.ereg = model
         self.create_gui()
 
     def create_gui(self) -> None:
@@ -23,4 +35,38 @@ class MainWindow(QMainWindow):
         self.resize(600, 600)
 
         pressure_setting_label = QLabel('Pressure Setting')
-        pressure_setting_le = QLineEdit()
+        self.pressure_reading_label = QLabel()
+        self.pressure_setting_le = QLineEdit()
+
+        # --- Create the layout ---
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        main_layout = QVBoxLayout(central_widget)
+
+        self.container_frame = QFrame()
+        self.container_frame.setFrameShape(QFrame.Shape.StyledPanel)
+        self.container_frame.setLineWidth(2)
+
+        frame_layout = QVBoxLayout(self.container_frame)
+        frame_layout.addWidget(pressure_setting_label)
+        frame_layout.addWidget(self.pressure_reading_label)
+        frame_layout.addWidget(self.pressure_setting_le)
+
+        main_layout.addWidget(self.container_frame)
+
+    def update_pressure_reading(self, pressure: float) -> None:
+        """
+        Updates the pressure reading on the UI.
+
+        Args:
+            pressure (float): The pressure reading from the e-reg in PSI.
+        """
+        pressure_mbar = h.convert_psi_to_mbar(pressure)
+        text = f'{pressure_mbar:.0f} mBar'
+        self.pressure_reading_label.setText(text)
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        self.closing_sig.emit()
+        event.accept()
+        super().closeEvent(event)
