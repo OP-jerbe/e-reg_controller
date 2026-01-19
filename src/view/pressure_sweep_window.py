e8924f252cfa70e4a1e276eae09dfe88d1e8a135
OP-jerbe <j.erbe@oregon-physics.com>
2026-01-19 11:28:59 -0800

    create the pressure sweep window

---
+++ src/view/pressure_sweep_window.py
@@ -0,0 +1,102 @@
+from PySide6.QtCore import QRegularExpression, Qt, Signal
+from PySide6.QtGui import QCloseEvent, QRegularExpressionValidator
+from PySide6.QtWidgets import (
+    QButtonGroup,
+    QGroupBox,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QRadioButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class PressureSweepWindow(QMainWindow):
+    pressure_sweep_sig = Signal(str, str, str)  # span, rate, direction
+
+    def __init__(self, parent: QWidget, current_pressure: int) -> None:
+        super().__init__(parent)
+        self.current_pressure = current_pressure
+        self.create_gui()
+        self.start_btn.setFocus()
+        self.start_clicked: bool = False
+
+    def create_gui(self) -> None:
+        # Set the window size
+        self.setWindowTitle('Pressure Sweep')
+        self.resize(300, 100)
+
+        # Create validator for ip and port inputs
+        entry_regex = QRegularExpression(r'[0-9.]*')
+        entry_validator = QRegularExpressionValidator(entry_regex)
+
+        # Create the widgets
+        self.span_label = QLabel('Pressure Span (mBar)')
+        self.span_entry = QLineEdit('400')
+        self.span_entry.setValidator(entry_validator)
+
+        self.rate_label = QLabel('Change Rate (mBar/sec)')
+        self.rate_entry = QLineEdit('2')
+        self.rate_entry.setValidator(entry_validator)
+
+        self.h2l_rb = QRadioButton('High-to-Low')
+        self.h2l_rb.setChecked(True)
+        self.l2h_rb = QRadioButton('Low-to-High')
+
+        self.rb_group = QButtonGroup()
+        self.rb_group.addButton(self.h2l_rb, 101)
+        self.rb_group.addButton(self.l2h_rb, 102)
+
+        self.start_btn = QPushButton('Start')
+        self.start_btn.clicked.connect(self.handle_start_clicked)
+        self.start_btn.setAutoDefault(True)
+
+        rb_group_box = QGroupBox('Sweep Direction')
+
+        # Set the layout
+        label_layout = QHBoxLayout()
+        label_layout.addWidget(self.span_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        label_layout.addWidget(self.rate_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        input_layout = QHBoxLayout()
+        input_layout.addWidget(self.span_entry)
+        input_layout.addWidget(self.rate_entry)
+
+        rb_group_layout = QHBoxLayout()
+        rb_group_layout.addWidget(self.h2l_rb)
+        rb_group_layout.addWidget(self.l2h_rb)
+
+        rb_group_box.setLayout(rb_group_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(label_layout)
+        main_layout.addLayout(input_layout)
+        main_layout.addWidget(rb_group_box)
+        main_layout.addWidget(self.start_btn)
+
+        self.setLayout(main_layout)
+
+        container = QWidget()
+        container.setLayout(main_layout)
+        self.setCentralWidget(container)
+
+    def handle_start_clicked(self) -> None:
+        self.start_clicked = True
+        self.close()
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        if self.start_clicked:
+            span = self.span_entry.text()
+            rate = self.rate_entry.text()
+            direction = ''
+            match self.rb_group.checkedId():
+                case 101:
+                    direction = 'H2L'
+                case 102:
+                    direction = 'L2H'
+            self.pressure_sweep_sig.emit(span, rate, direction)
+        event.accept()
+        super().closeEvent(event)
